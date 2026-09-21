bbd631e1cac49d6aca98bd0b41fd3ee5479de987
Bill O'Connell <91283923+wdoconnell@users.noreply.github.com>
2026-09-21 15:29:17 -0400

    fix: correct X-pos on second page

--- src/constants.ts
+++ src/constants.ts
@@ -5,12 +5,14 @@ export const POKEDEX_API_SOURCE_URL = "https://pokeapi.co/api/v2/pokemon/"
 
 // Player Header Indents, in Pixels
 export const PLAYER_NAME_AREA_INDENT_X = 150
-export const PLAYER_NAME_AREA_START_Y = 705
+export const PLAYER_NAME_AREA_START_Y_ONE = 705
+export const PLAYER_NAME_AREA_START_Y_TWO = 708
 
 // Form indents
 export const FORM_LEFT_INDENT_X = 95
 export const FORM_RIGHT_INDENT_X = 385
 export const POKEMON_START_LEFT_ONE = 610
+export const POKEMON_START_LEFT_TWO = 615
 export const DIST_NAME_TO_ALIGNMENT = 26
 export const DIST_ALIGNMENT_TO_ABILITY = 24
 export const DIST_ABILITY_TO_ITEM = 25
